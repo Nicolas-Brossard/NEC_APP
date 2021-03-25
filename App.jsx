208771ded91ccdbba35ab3ee22599f2a8c6db1e2
Neeko <brossard.nicolas09@gmail.com>
2021-03-25 15:06:24 +0100

    Add Sample

--- App.jsx
+++ App.jsx
@@ -1,13 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Sample from './Components/SampleComponent';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
@@ -16,6 +17,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Sample />
     </View>
   );
 }
